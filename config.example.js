ccfacb6596230e6f8470423cb02a5d4542d9fd8b
Chris Diana <cdiana.media@gmail.com>
2020-03-09 19:55:07 -0600

    add config example

---
+++ config.example.js
@@ -0,0 +1,11 @@
+// AWS COGNITO CONFIG EXAMPLE
+// Rename to config.js
+const config = {
+  userPoolId: '',
+  clientId: '',
+  identityPoolId: '',
+  identityPoolUrl: '',
+  region: '',
+};
+
+export default config;
